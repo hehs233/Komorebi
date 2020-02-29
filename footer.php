b1d484c1e632831977e3e6f72c2c2fab696d06a9
xiaotongxueya <59835969+xiaotongxueya@users.noreply.github.com>
2020-02-29 18:20:32 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,13 +2,11 @@
 <div id="footer">© 2016-<?php echo date('Y'); ?> <?php $this->author(); ?>. Theme by
 <a href="https://blog.fueis.com" target="_blank">Komorebi</a>
 , Powered by
-<a href="http://typecho.org/" target="_blank">Typecho</a>. 加载时间：0.00<?php echo timer_stop();?></br>
+<a href="http://typecho.org/" target="_blank">Typecho</a>. 加载时间：0.0<?php echo timer_stop();?></br>
 <a href="http://beian.miit.gov.cn/" target="_blank"><span>
 		
-            <?php if ($this->options->beian): ?>
-			<?php $this->options->beian() ?>
-            <?php else: ?>
-        	    <?php $this->options->description() ?>
+    <?php if ($this->options->beian): ?>
+	<?php $this->options->beian() ?>       	   
             <?php endif; ?>
 			
         </span></a>
