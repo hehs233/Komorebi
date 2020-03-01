21dcbcdb7be46b33b22d1d858e2fd3efd7e8e3ff
xiaotongxueya <59835969+xiaotongxueya@users.noreply.github.com>
2020-03-01 09:39:14 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <div id="footer">© 2016-<?php echo date('Y'); ?> <?php $this->author(); ?>. Theme by
-<a href="https://blog.fueis.com" target="_blank">Komorebi</a>
+<a href="https://www.xiaojunkang.com/" target="_blank">Komorebi</a>
 , Powered by
 <a href="http://typecho.org/" target="_blank">Typecho</a>. 加载时间：0.0<?php echo timer_stop();?></br>
 <a href="http://beian.miit.gov.cn/" target="_blank"><span>
